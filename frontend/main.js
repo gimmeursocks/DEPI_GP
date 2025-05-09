f5fef8c8ecb1bb8aaaa573cbbe62a25d060b144f
gimmeursocks <bobteen1@gmail.com>
2025-05-09 21:52:42 +0300

    New loadbalancer url (temporary)

--- frontend/main.js
+++ frontend/main.js
@@ -19,7 +19,7 @@ function displayInfoToast(message) {
     });
 }
 
-const API_BASE_URL = 'http://localhost:8000/';
+const API_BASE_URL = 'http://ab2881915a55643cc937f4121e3f8421-1218404674.eu-central-1.elb.amazonaws.com:8000/';
 
 function logout() {
     localStorage.removeItem('token');
